b222a28abcdaf5f100811acc50b6c329514ec50c
jessicalfry <137213294+jessicalfry@users.noreply.github.com>
2023-07-31 13:17:49 +0100

    adds homework files
    adds app.js, index.html, package-lock.json, package.json, script.js, style.css files to specialisationweek1 branch.

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server is on localhost:${port}`);
+});
+
+app.use(express.static(path.join(__dirname)));
+
---
+++ script.js
@@ -0,0 +1,11 @@
+function calculateSum() {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+
+ if (isNaN(num1) || isNaN(num2)) {
+    alert("Please enter a valid numeric value.");
+ } else {
+    const sum = num1 + num2;
+    document.getElementById('result').textContent = `Result: ${sum}`;
+ }
+}
